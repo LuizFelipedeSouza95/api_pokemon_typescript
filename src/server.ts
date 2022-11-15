be18efb8ee7f224dd0825f154d4d243fc9bdb332
LuizFelipe883 <luizfelipedesouza94@gmail.com>
2022-11-15 19:21:57 -0300

    deploy

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,12 @@ import express, { json } from "express";
 import { router } from "./routes";
 require('dotenv').config()
 
-const port = process.env.PORT||'3003';
+const PORT = process.env.PORT || '3003';
 const app = express();
 
 app.use(json());
 app.use(router);
 
-app.listen(port, async () => {
-  console.info(`⚡️Server is running at http://localhost:${port}`);
+app.listen(PORT, async () => {
+  console.info(`⚡️Server is running at http://localhost:${PORT}`);
 });
