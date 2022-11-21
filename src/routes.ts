2e2c5ca13f9a406326e2907a6b0172744036b52b
LuizFelipe883 <luizfelipedesouza94@gmail.com>
2022-11-20 21:25:54 -0300

    swagger

--- src/routes.ts
+++ src/routes.ts
@@ -2,12 +2,15 @@ import express from "express";
 import { Controllers } from "./controllers/controllerCrud";
 import { controllerBattlePokemon } from "./controllers/controllerBattlePokemon";
 import { controllerCurePokemon } from "./controllers/controllerCurePokemon";
+import { serve,setup } from "swagger-ui-express"
+import * as swagger from "./swagger.json";
 
 const router = express.Router();
 const controller = new Controllers();
 const battle = new controllerBattlePokemon()
 const cure = new controllerCurePokemon()
 
+router.use('/api/pokemon/docs', serve, setup(swagger));
 router.post("/api/pokemon/create", controller.createPokemon);
 router.get("/api/pokemon/searchAll", controller.searchAllPokemons);
 router.get("/api/pokemon/searchOne", controller.searchOnePokemon);
